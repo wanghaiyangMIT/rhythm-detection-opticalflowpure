1add99df5cd14398417a488d5a15fb992f6e6e25
wanghaiyangMIT <37353864+wanghaiyangMIT@users.noreply.github.com>
2018-12-16 21:35:33 +0800

    Add files via upload

---
+++ Resnet34.py
@@ -0,0 +1,103 @@
+import torch
+import torch.nn as nn            #basic model
+import torch.utils.data as data  #load data
+import torchvision               #something in vision
+import torchvision.transforms as transforms
+from tqdm import tqdm
+import time
+import math
+
+def conv3x3(input_layer, output_layer, stride = 1):
+    '3x3 convolution with padding'
+    return nn.Conv2d(input_layer,output_layer,kernel_size = 3,stride = stride,
+                     padding = 1,bias = False)
+
+class BasicBlock(nn.Module):
+    expansion = 1
+    
+    def __init__(self,inplanes,planes,stride = 1,downsample = None):
+        super(BasicBlock,self).__init__()
+        self.conv1 = conv3x3(inplanes,planes,stride)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.relu = nn.ReLU(inplace = True)
+        self.conv2 = conv3x3(planes, planes)
+        self.bn2 = nn.BatchNorm2d(planes)
+        self.downsample = downsample
+        self.stride = stride
+    
+    def forward(self, x):
+        residual = x
+
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        if self.downsample is not None:
+            residual = self.downsample(x)
+
+        out += residual
+        out = self.relu(out)
+
+        return out
+
+
+class Resnet34(nn.Module):
+    def __init__(self, block, layers, num_classes=10):
+        self.inplanes = 64
+        super(Resnet34, self).__init__()
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, padding=3,
+                               bias=False) #input 224x224, output 224x224
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)#112x112  
+        self.layer1 = self._make_layer(block, 64, layers[0],stride=2)#56x56
+        self.layer2 = self._make_layer(block, 128, layers[1], stride=2)#128x28x28
+        self.layer3 = self._make_layer(block, 256, layers[2], stride=2)#256x14x14
+        self.layer4 = self._make_layer(block, 512, layers[3], stride=2)#512x7x7
+        self.avgpool = nn.AvgPool2d(7, stride=1)
+        self.fc = nn.Linear(512 * block.expansion, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                n = m.kernel_size[0] * m.kernel_size[1] * m.out_channels
+                m.weight.data.normal_(0, math.sqrt(2. / n))
+            elif isinstance(m, nn.BatchNorm2d):
+                m.weight.data.fill_(1)
+                m.bias.data.zero_()
+
+    def _make_layer(self, block, planes, blocks, stride=1):
+        downsample = None
+        if stride != 1 or self.inplanes != planes * block.expansion:
+            downsample = nn.Sequential(
+                nn.Conv2d(self.inplanes, planes * block.expansion,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(planes * block.expansion),
+            )
+
+        layers = []
+        layers.append(block(self.inplanes, planes, stride, downsample))
+        self.inplanes = planes * block.expansion
+        for i in range(1, blocks):
+            layers.append(block(self.inplanes, planes))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = self.relu(x)
+        x = self.maxpool(x)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+        x = self.layer4(x)
+
+        x = self.avgpool(x)
+        x = x.view(x.size(0), -1)
+        x = self.fc(x)
+
+        return x
